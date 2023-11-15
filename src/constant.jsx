f561a1a4f121e0828f514983363173a60f59ddf1
SK ZAHIRUL ISLAM <skzahirulislam22@gmail.com>
2023-11-15 18:37:55 +0530

    removed proxy

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,14 +1,8 @@
 export const IMG_CDN_URL =
 	"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-// export const FETCH_RESTAURANTS =
-// 	"https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-
-// export const FETCH_RESTAURANT_DETAILS =
-// 	"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId=";
-
 export const FETCH_RESTAURANTS =
-	"/restaurants?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+	"https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const FETCH_RESTAURANT_DETAILS =
-	"/menu?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId=";
+	"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId=";
--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	server: {
-		proxy: {
-			"/restaurants": {
-				target: "https://www.swiggy.com/dapi/restaurants/list/v5",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/restaurants/, ""),
-			},
-			"/menu": {
-				target: "https://www.swiggy.com/dapi/menu/pl",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/menu/, ""),
-			},
-		},
-	},
 	plugins: [react()],
 });
