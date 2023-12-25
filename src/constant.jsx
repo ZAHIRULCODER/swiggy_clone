957708483769e0fa47450e593c1becbe381062ef
SK ZAHIRUL ISLAM <skzahirulislam22@gmail.com>
2023-12-25 16:27:56 +0530

    Fix IMG_CDN_URL constant in constant.jsx

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,7 +1,6 @@
 const CORSPROXY = "https://thingproxy.freeboard.io/fetch/";
 
-export const IMG_CDN_URL =
-	"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const IMG_CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
 
 //Cors fixed
 export const FETCH_RESTAURANTS = `${CORSPROXY}https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
