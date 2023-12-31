572cdac9a08b235e1a231e3a0f5b5c55891250c0
SK ZAHIRUL ISLAM <skzahirulislam22@gmail.com>
2023-12-31 23:52:06 +0530

    Update CORS proxy URL in constant.jsx

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,8 +1,12 @@
-const CORSPROXY = "https://cors-anywhere.herokuapp.com/";
+const CORSPROXY = "https://corsproxy.io/?";
 
 export const IMG_CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
 
 //Cors fixed
-export const FETCH_RESTAURANTS = `${CORSPROXY}https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+export const FETCH_RESTAURANTS =
+	CORSPROXY +
+	"https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const FETCH_RESTAURANT_DETAILS = `${CORSPROXY}https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001`;
+export const FETCH_RESTAURANT_DETAILS =
+	CORSPROXY +
+	"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001";
