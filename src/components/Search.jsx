91bf60284d6983c6c5e7582710b45cc46e3e821b
ZAHIRULCODER <skzahirulislam22@gmail.com>
2025-09-28 18:48:17 +0530

    Refactor Search component to use useEffect for filtering restaurants and remove unused search button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import RestaurantContext from "../context/RestaurantContext";
 import { filterData } from "../utils/helperFunc";
 
@@ -7,28 +7,20 @@ export function Search() {
    const { allRestaurants, setFilteredRestaurant } =
       useContext(RestaurantContext);
 
+   useEffect(() => {
+      const data = filterData(searchText, allRestaurants);
+      setFilteredRestaurant(data);
+   }, [searchText, allRestaurants, setFilteredRestaurant]);
+
    return (
-      <div className="flex mx-auto items-center space-x-1">
+      <div className="flex mx-auto items-center">
          <input
-            className="w-[350px] border p-2 rounded-l-lg focus:outline-none"
+            className="w-[400px] border p-2 rounded-lg focus:outline-none"
             type="text"
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
             placeholder="Search for restaurants"
          />
-         <button
-            onClick={() => {
-               //need to filter data
-               const data = filterData(searchText, allRestaurants);
-               //update the state - restaurants
-               setFilteredRestaurant(data);
-               setSearchText("");
-            }}
-            className="bg-slate-600 text-white p-2 rounded-r-lg hover:bg-black focus:outline-none"
-            type="submit"
-         >
-            Search
-         </button>
       </div>
    );
 }
